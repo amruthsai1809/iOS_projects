12e9a422536cf73578b2d6887af4422bef95e505
amruthsai1809 <amruthsai1809@gmail.com>
2017-08-18 17:53:08 -0400

    Added gameOfChats

---
+++ gameofchats/gameofchats/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  gameofchats
+//
+//  Created by Amruth on 6/29/16.
+
+
+import UIKit
+
+class User: NSObject {
+    var id: String?
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    init(dictionary: [String: AnyObject]) {
+        self.id = dictionary["id"] as? String
+        self.name = dictionary["name"] as? String
+        self.email = dictionary["email"] as? String
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+    }
+}
